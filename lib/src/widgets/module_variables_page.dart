01684d5b4ffeaf4da0d1269361efd5145daa472e
Chris Norman <chris.norman1989@outlook.com>
2025-08-17 17:17:23 +0100

    Start using `flutter_form_builder`.

--- lib/src/widgets/module_variables_page.dart
+++ lib/src/widgets/module_variables_page.dart
@@ -1,7 +1,9 @@
 import 'package:backstreets_widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_open_scad/flutter_open_scad.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 /// The page which shows module variables.
 class ModuleVariablesPage extends ConsumerWidget {
@@ -31,18 +33,42 @@ class ModuleVariablesPage extends ConsumerWidget {
         autofocus: true,
       );
     }
-    return ListView.builder(
-      itemBuilder: (final context, final index) {
-        final variable = variables[index];
-        return ListTile(
-          autofocus: index == 0,
-          title: Text(variable.name),
-          subtitle: Text('${variable.defaultValue}'),
-          onTap: () {},
-        );
-      },
-      itemCount: variables.length,
-      shrinkWrap: true,
+    final formKey = GlobalKey<FormBuilderState>();
+    return FormBuilder(
+      key: formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (var i = 0; i < variables.length; i++)
+              () {
+                final variable = variables[i];
+                return FormBuilderTextField(
+                  name: variable.id,
+                  autofocus: i == 0,
+                  decoration: InputDecoration(labelText: variable.name),
+                  initialValue: variable.defaultValue.toStringAsFixed(5),
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                  ]),
+                  valueTransformer: (final value) =>
+                      value == null || value.isEmpty
+                      ? variable.defaultValue
+                      : double.parse(value),
+                );
+              }(),
+            IconButton(
+              onPressed: () {
+                formKey.currentState?.saveAndValidate();
+                print(formKey.currentState?.value);
+              },
+              icon: const Icon(Icons.save),
+              tooltip: 'Save',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
