43c2e958cd1590276b2ac5b08534eabaaa900b9f
Chris Norman <chris.norman1989@outlook.com>
2025-08-24 18:26:02 +0100

    Use the new sorting.

--- lib/src/widgets/module_variables_page.dart
+++ lib/src/widgets/module_variables_page.dart
@@ -26,11 +26,9 @@ class ModuleVariablesPage extends ConsumerWidget {
   @override
   Widget build(final BuildContext context, final WidgetRef ref) {
     final projectContext = ref.watch(projectProvider(projectFilename));
-    final variables =
-        ref.watch(moduleVariablesProvider(projectFilename, moduleId))..sort(
-          (final a, final b) =>
-              a.name.toLowerCase().compareTo(b.name.toLowerCase()),
-        );
+    final variables = ref.watch(
+      moduleVariablesProvider(projectFilename, moduleId),
+    );
     if (variables.isEmpty) {
       return const CenterText(
         text: 'This module has no variables.',
@@ -42,6 +40,48 @@ class ModuleVariablesPage extends ConsumerWidget {
     return ListView.builder(
       itemBuilder: (final context, final index) {
         final variable = variables[index];
+
+        String getDescription() {
+          final firstVariableId = variable.firstVariableId;
+          final secondVariableId = variable.secondVariableId;
+          final variable1 = firstVariableId == null
+              ? null
+              : allVariables.firstWhere((final v) => v.id == firstVariableId);
+          final variable2 = secondVariableId == null
+              ? null
+              : allVariables.firstWhere((final v) => v.id == secondVariableId);
+          final x = variable1?.name ?? (variable.firstValue.toStringAsFixed(5));
+          final y =
+              variable2?.name ?? (variable.secondValue.toStringAsFixed(5));
+          switch (variable.operation) {
+            case VariableOperation.verbatim:
+              return x;
+            case VariableOperation.add:
+              return '$x + $y';
+            case VariableOperation.subtract:
+              return '$x - $y';
+            case VariableOperation.multiply:
+              return '$x * $y';
+            case VariableOperation.divide:
+              return '$x / $y';
+            case VariableOperation.power:
+              return '$x ^ $y';
+            case VariableOperation.cos:
+              return 'cos($x)';
+            case VariableOperation.sin:
+              return 'sin($x)';
+            case VariableOperation.tan:
+              return 'tan($x)';
+            case VariableOperation.acos:
+              return 'acos($x)';
+            case VariableOperation.asin:
+              return 'asin($x)';
+            case VariableOperation.atan:
+              return 'atan($x)';
+          }
+        }
+
+        final value = module.getVariableValue(variable);
         return PerformableActionsListTile(
           actions: [
             PerformableAction(
@@ -71,48 +111,7 @@ class ModuleVariablesPage extends ConsumerWidget {
           ],
           autofocus: index == 0,
           title: Text(variable.name),
-          subtitle: Text(() {
-            final firstVariableId = variable.firstVariableId;
-            final secondVariableId = variable.secondVariableId;
-            final variable1 = firstVariableId == null
-                ? null
-                : allVariables.firstWhere((final v) => v.id == firstVariableId);
-            final variable2 = secondVariableId == null
-                ? null
-                : allVariables.firstWhere(
-                    (final v) => v.id == secondVariableId,
-                  );
-            final x =
-                variable1?.name ?? (variable.firstValue.toStringAsFixed(5));
-            final y =
-                variable2?.name ?? (variable.secondValue.toStringAsFixed(5));
-            switch (variable.operation) {
-              case VariableOperation.verbatim:
-                return x;
-              case VariableOperation.add:
-                return '$x + $y';
-              case VariableOperation.subtract:
-                return '$x - $y';
-              case VariableOperation.multiply:
-                return '$x * $y';
-              case VariableOperation.divide:
-                return '$x / $y';
-              case VariableOperation.power:
-                return '$x ^ $y';
-              case VariableOperation.cos:
-                return 'cos($x)';
-              case VariableOperation.sin:
-                return 'sin($x)';
-              case VariableOperation.tan:
-                return 'tan($x)';
-              case VariableOperation.acos:
-                return 'acos($x)';
-              case VariableOperation.asin:
-                return 'asin($x)';
-              case VariableOperation.atan:
-                return 'atan($x)';
-            }
-          }()),
+          subtitle: Text('${getDescription()} = $value'),
           onTap: () => context.pushWidgetBuilder(
             (final innerContext) => EditVariableScreen(
               projectFilename: projectFilename,
